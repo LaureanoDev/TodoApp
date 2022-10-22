760ba934b483cba6ee843de1783e77b7ccfa1af1
LaureanoDev <laureanoperezlindodev@hotmail.com>
2022-10-22 12:13:35 -0300

    fixed errors and add importants todos

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,18 @@ function App() {
 
   const addTodo = (text) => {
     let id = 1;
-    if (todos.lenght > 0) {
-      id = todos[0].id + 1;
+    if(todos.length > 0) {
+      id = todos[0].id + 1
     }
-    let todo = { id: id, text: text, completed: false };
-    let newTodos = [todo, ...todos];
-    setTodos(newTodos);
+    let todo = {id: id, text: text, completed: false, important: false}
+    let newTodos = [todo, ...todos]
+    setTodos(newTodos)
   };
-  
+
   const removeTodo = (id) => {
-    let updatedTodos = [...todos].filter((todo) => todo.id !== id)
-    setTodos(updatedTodos)
-  }
+    let updatedTodos = [...todos].filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
 
   const completeTodo = (id) => {
     let updatedTodos = todos.map((todo) => {
@@ -30,17 +30,30 @@ function App() {
     })
     setTodos(updatedTodos)
   }
+
+  const importantTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if(todo.id === id) {
+        todo.important = !todo.important
+      }
+      return todo
+    })
+
+    setTodos(updatedTodos)
+  }
+  let sortedTodos = todos.sort((a, b) => b.important - a.important)
   return (
     <div className="todo-app">
-      <h1>Todo List</h1>
-      <TodoForm addTodo={addTodo} />
-      {todos.map((todo) => {
-        return (
-          <TodoItem completeTodo={completeTodo} removeTodo={removeTodo} todo={todo} key={todo.id}/>
-        )
-      })}
-    </div>
-  );
+    <h1>Todo List</h1>
+    <TodoForm addTodo={addTodo} />
+    <hr className="seperator"/>
+    {sortedTodos.map((todo) => {
+      return (
+        <TodoItem removeTodo={removeTodo} completeTodo={completeTodo} importantTodo={importantTodo} todo={todo} key={todo.id}/>
+      )
+    })}
+  </div>
+);
 }
 
 export default App;
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { RiCloseCircleLine } from "react-icons/ri";
 import { BiCheckCircle } from "react-icons/bi";
 
-const TodoItem = ({ todo, removeTodo, completeTodo }) => {
+const TodoItem = ({ todo, removeTodo, completeTodo, importantTodo }) => {
   return (
-    <div className={todo.completed ? 'todo-row complete' : 'todo-row'}>
-      {todo.text}
-      <div className="iconsContainer">
-        <RiCloseCircleLine style={{ marginRight: 5 }} onClick={() => removeTodo(todo.id)}/>
-        <BiCheckCircle onClick={() => completeTodo(todo.id)}/>
-      </div>
-    </div>
+    <div className={todo.completed ? "todo-row complete" : "todo-row"} style={todo.important ? { background: "orange" } : {}}>
+            {todo.text}
+            <div className="iconsContainer">
+                <button onClick={() => importantTodo(todo.id)} className="important-btn">!</button>
+                <RiCloseCircleLine style={{ marginRight: 5 }} onClick={() => removeTodo(todo.id)}/>
+                <BiCheckCircle onClick={() => completeTodo(todo.id)}/>
+            </div>
+        </div>
   );
 };
 
